1e0f4b3b4d17ea6ac881036323882d06ce672b8f
ldalrymple1 <dalrymple.lydia@gmail.com>
2020-04-25 11:45:16 +0100

    Adding the aliens.

--- app.js
+++ app.js
@@ -6,15 +6,56 @@ document.addEventListener('DOMContentLoaded', () => {
   const width = 10
   const height = 10
   const squares = []
+  let playerIndex = 95
+
   
   for (let i = 0; i < width ** 2; i++) {
     const square = document.createElement('div')
-    square.setAttribute('data-id', i)
+    square.setAttribute('square-id', i)
   
     grid.appendChild(square)
     squares.push(square)
     square.classList.add('.div')
   }
+
+  function setUp() {
+    console.log(squares)
+    squares.forEach((elem, i) => {
+      if (i === playerIndex) {
+        elem.classList.add('player')
+      }
+      if (i >= 0 && i <= 29) {
+        elem.classList.add('alien')
+      }
+
+    })
+  }
+
+  setUp()
+
+
+  document.addEventListener('keyup', (e) => {
+    
+    // function movePlayer(event) {
+    //   console.log('working')
+    //   let x = event.keyCode
+    //   if (x === 38) {
+    //     console.log('38')
+    //     return playerIndex -= width
+    //   }
+    // }
+  
+    // setUp()
+    // movePlayer()
+  
+
+
+  })
+
+  
+
+
+
 
 
 
